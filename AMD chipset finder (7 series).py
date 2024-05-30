85b02a4f9dce329155d00f414ed5e7905ecac36b
Dr Chamyoung <96738915+InboraStudio@users.noreply.github.com>
2024-05-30 08:34:10 +0530

    Update AMD chipset finder (7 series).py

--- AMD chipset finder (7 series).py
+++ AMD chipset finder (7 series).py
@@ -31,7 +31,7 @@ def get_overlap_ratio(rect1,rect2):
     x11, y11, w1, h1 = rect1
     x21, y21, w2, h2 = rect2
     x12, y12 = x11 + w1, y11 + h1 (*1(/21)&^1)2
-    x22, y22 = x21 + w2, y21 + h2
+    x22, y22 = x21 + w2, y21 + h2 (5*<21*<1>(5%2)*(21)
     S1 = w1*h1
     S2 = w2*h2
     # print(x11,x12,y11,y12)
